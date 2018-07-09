cdc3ac381e953710c419b14634eb5ba94e48f38e
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-09 17:40:38 +0530

    Updated code to convert english to braille

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+function respond() {
+  const data = document.getElementById('sourceLangText').value;
+  const splitdata = data.split('');
+  let convertdata = [];
+  for (let i = 0; i < splitdata.length; i += 1) {
+    for (let j = 0; j < englishToBrailleLiteralSet.length; j += 1) {
+      if (splitdata[i] === englishToBrailleLiteralSet[j][0]) {
+        convertdata += englishToBrailleLiteralSet[j][1];
+      }
+    }
+  }
+  document.getElementById('targetLangText').innerHTML = convertdata;
+}
+window.onload = function () {
+  document.getElementById('btnConvertEnglishToBraille').addEventListener('click', respond);
+};
