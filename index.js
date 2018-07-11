220219ae4c0d028b6b11480bbee7c2133424f300
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-11 09:22:04 +0530

    added comments

--- index.js
+++ index.js
@@ -8,15 +8,16 @@
 import englishToBrailleLiteralSet from './english-to-braille.js';
 
 function respond() {
-  const brailmap = new Map(englishToBrailleLiteralSet);
-  const data = document.getElementById('sourceLangText').value;
-  const splitdata = data.split('');
+  const brailmap = new Map(englishToBrailleLiteralSet); //  convert 2D to MAP's
+  const data = document.getElementById('sourceLangText').value; //  get input text from div
+  const splitdata = data.split(''); //  convert text to array
   let condata = [];
   splitdata.forEach((element) => {
     condata += brailmap.get(element);
   });
   document.getElementById('targetLangText').innerHTML = condata;
 }
+//  Wait till page gets loaded
 window.onload = function () {
   document.getElementById('btnConvertEnglishToBraille').addEventListener('click', respond);
 };
